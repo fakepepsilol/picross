31111994a789314f09bd97d09b83df5ddfaa8c6b
fakepepsilol <fakepepsilol@gmail.com>
2025-10-17 15:59:43 +0200

    finished (already?)

--- src/main/kotlin/rs/fpl/Main.kt
+++ src/main/kotlin/rs/fpl/Main.kt
@@ -1,5 +1,10 @@
 package rs.fpl
 
+import okio.Buffer
+import okio.InflaterSource
+import okio.inflate
+import java.util.zip.Inflater
+import kotlin.io.encoding.Base64
 
 fun main(){
     print("base64: ")
@@ -11,6 +16,18 @@ fun main(){
             else -> c
         }
     }.joinToString("")
-    println("fixed:  ${b64}")
 
+    val bytes: ByteArray = Base64.decode(b64)
+    val source = Buffer().apply { write(bytes) }
+
+    val inflaterSource: InflaterSource = source.inflate(Inflater(true))
+    val sink = Buffer()
+    var bytesRead = 0L
+    while(true){
+        val ret = inflaterSource.read(sink, Long.MAX_VALUE)
+        if(ret < 0) break
+        bytesRead += ret
+    }
+    val output = sink.readByteArray().decodeToString()
+    println(output)
 }
